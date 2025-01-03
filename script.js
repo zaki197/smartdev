071ac5b76bbbc0afc9f1fdd6bdfed8b15236e575
zaki197 <bx1.alg@gmail.com>
2025-01-03 10:54:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('windows-button').addEventListener('click', function() {
+  window.location.href = 'https://www.dropbox.com/scl/fi/xsqp9rqoioljbjtc8w5yf/Gestion-de-Cabinet-M-dicalx32bit-Username-admin-Password-123456.exe?rlkey=9kkeyc4tb10ip2f6mnat70vhc&st=s4wnil1g&dl=1'; // Update this with your app's download link for Windows
+});
+
+document.getElementById('mac-button').addEventListener('click', function() {
+  window.location.href= 'https://www.dropbox.com/scl/fi/4hu4n5odrxqes9k61nolm/Gestion-de-Cabinet-M-dicalx64bit-Username-admin-Password-123456.exe?rlkey=zuayt0itabv00lji935481i7a&st=fz7xa6ci&dl=1'; // Update with your Mac download link
+});
+
