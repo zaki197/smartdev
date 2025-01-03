bcf538f09e6a28cbfc1761824109ac0a32c364b5
zaki197 <bx1.alg@gmail.com>
2025-01-03 10:38:44 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('windows-button').addEventListener('click', function() {
-  window.location.href = 'https://download1640.mediafire.com/d97us68v2vxgW3yoizxjs8Z2-D9VvAboLO9M4E1J9-iVZEanWutwrGTlqgQFskdVzvt6byVY5w1PSjezLkdGtkL1kVZV84Xs9vvFDhhpkxL5RsQUsfWb7dS6FKFbgLP6kFQDdFNf1BRABqck-cVWUnbDtxwBLcrY3OwADmtaHVpR/v6z35fxukxdaam2/Gestion+de+Cabinet+M%C3%A9dicalx32bit%28Username%3Dadmin%2CPassword%3D123456%29.exe'; // Update this with your app's download link for Windows
-});
-
-document.getElementById('mac-button').addEventListener('click', function() {
-  window.location.href= 'https://download1529.mediafire.com/zq8brjblijdgDiT4CdufzDZUBoJscXKsHcKXQTSSV4kG2fRHkP4HW3BQkUu7H1gEjspdve1kIPnBZgPQBHzehJOClrfiuoCadXRobQ2H-At1Zc9Ls2_JPxWTQhvO_2rRwVoJUw5CU4Mh4jCMh5zQm3UjNuAUBpAtLszFsYFFI5Hl/zkq10pkecw9zqe0/Gestion+de+Cabinet+M%C3%A9dicalx64bit%28Username%3Dadmin%2CPassword%3D123456%29.exe'; // Update with your Mac download link
-});
-
