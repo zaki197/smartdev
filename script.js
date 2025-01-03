958c5f6b8667f5be25ac125788b03b01751dcbf1
zaki197 <bx1.alg@gmail.com>
2025-01-03 10:40:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('windows-button').addEventListener('click', function() {
+  window.location.href = 'https://uc12a00479fe94b988a34179dc7e.dl.dropboxusercontent.com/cd/0/get/ChfUzF0XgLnybZ5Q4r51qxHwozYs_ygSzyW7SZTVnFRIL45re9qn-e7G1u6SS-V41lXeIxNb2jsMbkoWcOYry0QZRO6ohC-uTsA_4DRWc090OSIAHBSyIfHVjv8PChRaRFJO6Qbz9WRQ3DImNRrmGGFRdrIzEY1zdZzAEtGhif58fA/file?_download_id=8180337019771708907015323638456215697821159843829786093958462574&_log_download_success=1#'; // Update this with your app's download link for Windows
+});
+
+document.getElementById('mac-button').addEventListener('click', function() {
+  window.location.href= 'https://uca2f117902030a8947a439d4a2b.dl.dropboxusercontent.com/cd/0/get/CheqyZa71ZOEdR4lcOs27e_Er-bwJZuLoAZSTnBrGK8XCA9sbWp7-dHgE6PTRukPDD2h-zNQzE62hh64fOsRk7GRnHaI8UMFbz0O70c_zQwMG2xQcY0wXTyFmTPjhU2p49CRPhoFtrPETzTf06SryAk1koWmcmP5KVEQO0TmKg1mVw/file?_download_id=30655399925801968985317389645113919390265901950712016719291372324&_log_download_success=1#'; // Update with your Mac download link
+});
+
